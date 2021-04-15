ac856ad0cedb7fd76bd2ddc4a2d0a638ec12629c
mmccauley <missa444@gmail.com>
2021-04-15 11:25:11 -0400

    started new Students app, and added functionality to enable users to enroll in courses

--- educa/settings.py
+++ educa/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +35,10 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my apps
     'courses.apps.CoursesConfig',
+    'students.apps.StudentsConfig',
+    # third party apps
+    'sslserver',
+    'embed_video',
     # django apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from django.urls import reverse_lazy
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
